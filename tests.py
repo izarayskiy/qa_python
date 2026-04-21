1e604208997fb7fc3d91d7e0a2dfad62f45e4ac6
Ilia Zarayskiy <i.zarayskiy@proton.me>
2026-04-21 23:08:57 +0300

    Added a fixture. Removed the BooksCollector class initialization in each test

--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
 import pytest
 
-from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -10,10 +9,7 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
+    def test_add_new_book_add_two_books(self, collector):
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
@@ -32,10 +28,8 @@ class TestBooksCollector:
             'А' * 40,
         ],
     )
-    def test_add_new_book_with_valid_name_length(self, name):
+    def test_add_new_book_with_valid_name_length(self, name, collector):
         """Добавляем книгу с корректным именем"""
-        collector = BooksCollector()
-
         collector.add_new_book(name)
 
         assert name in collector.get_books_genre()
@@ -48,26 +42,22 @@ class TestBooksCollector:
             'B' * 41,
         ],
     )
-    def test_add_new_book_with_invalid_name_length(self, name):
+    def test_add_new_book_with_invalid_name_length(self, name, collector):
         """Добавляем книгу с некорректным именем"""
-        collector = BooksCollector()
-
         collector.add_new_book(name)
 
         assert name not in collector.get_books_genre()
 
-    def test_set_book_genre_for_existing_book(self):
+    def test_set_book_genre_for_existing_book(self, collector):
         """Получаем жанр книги"""
-        collector = BooksCollector()
         collector.add_new_book('Дюна')
 
         collector.set_book_genre('Дюна', 'Фантастика')
 
         assert collector.get_book_genre('Дюна') == 'Фантастика'
 
-    def test_get_books_with_specific_genre(self):
+    def test_get_books_with_specific_genre(self, collector):
         """Получаем книги по жанрам"""
-        collector = BooksCollector()
         collector.add_new_book('Дюна')
         collector.add_new_book('Оно')
         collector.set_book_genre('Дюна', 'Фантастика')
@@ -77,9 +67,8 @@ class TestBooksCollector:
 
         assert books == ['Дюна']
     
-    def test_get_books_genre_returns_genre_dictionary(self):
+    def test_get_books_genre_returns_genre_dictionary(self, collector):
         """Получаем словарь в котором содержатся книги и их жанры"""
-        collector = BooksCollector()
         collector.add_new_book('Дюна')
         collector.set_book_genre('Дюна', 'Фантастика')
 
@@ -87,9 +76,8 @@ class TestBooksCollector:
 
         assert books_genre == {'Дюна': 'Фантастика'}
 
-    def test_get_books_for_children(self):
+    def test_get_books_for_children(self, collector):
         """Получаем список книг для детей"""
-        collector = BooksCollector()
         collector.add_new_book('Дюна')
         collector.add_new_book('Шерлок Холмс')
         collector.add_new_book('Винни-Пух')
@@ -101,9 +89,8 @@ class TestBooksCollector:
 
         assert books_for_children == ['Дюна', 'Винни-Пух']
     
-    def test_add_book_in_favorites(self):
+    def test_add_book_in_favorites(self, collector):
         """Добавляем книгу в избранное"""
-        collector = BooksCollector()
         collector.add_new_book('Дюна')
 
         collector.add_book_in_favorites('Дюна')
@@ -112,9 +99,8 @@ class TestBooksCollector:
 
         assert collector.get_list_of_favorites_books() == ['Дюна']
     
-    def test_delete_book_from_favorites(self):
+    def test_delete_book_from_favorites(self, collector):
         """Удаляем книгу из избранного"""
-        collector = BooksCollector()
         collector.add_new_book('Дюна')
         collector.add_book_in_favorites('Дюна')
 
@@ -122,9 +108,8 @@ class TestBooksCollector:
 
         assert collector.get_list_of_favorites_books() == []
 
-    def test_get_current_list_of_favorites_books(self):
+    def test_get_current_list_of_favorites_books(self, collector):
         """Получаем список текущих книг в избранном"""
-        collector = BooksCollector()
         collector.add_new_book('Дюна')
         collector.add_new_book('Оно')
         collector.add_book_in_favorites('Дюна')
